d9c5506cc73e839eb1b507e9ce2d3d4370663c45
DenisKorotchenko <dzd1024ozd@gmail.com>
2026-01-22 19:50:31 +0300

    Add for collaborative

--- java/scripts/run2.sh
+++ java/scripts/run2.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-thread="4"
-size="16384"
+thread="1 2 4 8 15 31 47 63"
+# thread="1 4 8"
+size="1000000"
 
-writes="0 40 100"
+writes="0 20 50 100"
+# writes="20"
 
 ###############################
 # records all benchmark outputs
 ###############################
-snapshots="10 50"
-benchs="trees.lockbased.IntegerCollaborativeLockBasedStanfordTreeMap trees.lockbased.IntegerLockBasedStanfordTreeMap" #trees.lockbased.IntegerPureLockBasedStanfordTreeMap"
-#benchs="hashtables.lockbased.IntegerCollaborativeQueueHashMap hashtables.lockbased.IntegerQueueHashMap" #hashtables.lockbased.IntegerNonCollaborativeQueueHashMap
+snapshots="0 1 2 4 8 16 32 64 100"
+# snapshots="8"
+#benchs="trees.lockbased.IntegerCollaborativeLockBasedStanfordTreeMap trees.lockbased.IntegerLockBasedStanfordTreeMap" #trees.lockbased.IntegerPureLockBasedStanfordTreeMap"
+# benchs="hashtables.lockbased.IntegerCollaborativeQueueHashMap hashtables.lockbased.IntegerQueueHashMap hashtables.lockbased.IntegerNonCollaborativeQueueHashMap"
+benchs="hashtables.lockbased.ReduceCollaborative hashtables.lockbased.ReduceSimple hashtables.lockbased.ReduceUnlinearizable"
 for s in ${snapshots}; do
   for t in ${thread}; do
     for write in ${writes}; do
@@ -18,9 +22,9 @@ for s in ${snapshots}; do
         u=$((((100-$s)*$write)/100))
         for i in ${size}; do
           r=$((i*2))
-          out=/home/dkorotchenko/collaborative-operations/synchrobench-collaborative/java/output/log/${bench}-i${i}-u${u}-s${s}-t${t}.log
-          echo "taskset -c 0-15 java -server -cp ../lib/compositional-deucestm-0.1.jar:../lib/mydeuce.jar:../bin contention.benchmark.Test -W 5 -a 0 -s ${s} -d 5000 -t ${t} -i ${i} -r ${r} -n 5 -b ${bench} -u ${u} >> ${out}"
-          taskset -c 0-15 java -server -cp ../lib/compositional-deucestm-0.1.jar:../lib/mydeuce.jar:../bin contention.benchmark.Test -W 5 -a 0 -s ${s} -d 5000 -t ${t} -i ${i} -r ${r} -n 5 -b ${bench} -u ${u} --csvPath /home/dkorotchenko/collaborative-operations/synchrobench-collaborative/java/output/results.csv >> ${out}
+          out=/home/dkorotchenko/collaborative-operations/synchrobench-collaborative/java/output/log/071225/${bench}-i${i}-u${u}-s${s}-t${t}.log
+          echo "taskset -c 0-63 java -server -cp ../lib/compositional-deucestm-0.1.jar:../lib/mydeuce.jar:../bin contention.benchmark.Test -W 5 -a 0 -s ${s} -d 5000 -t ${t} -i ${i} -r ${r} -n 5 -b ${bench} -u ${u} >> ${out}"
+          taskset -c 0-63 java -server -cp ../lib/compositional-deucestm-0.1.jar:../lib/mydeuce.jar:../bin contention.benchmark.Test -W 5 -a 0 -s ${s} -d 5000 -t ${t} -i ${i} -r ${r} -n 5 -b ${bench} -u ${u} --csvPath /home/dkorotchenko/collaborative-operations/synchrobench-collaborative/java/output/results.csv >> ${out}
         done
       done
     done
---
+++ java/src/hashtables/lockbased/ReduceUnlinearizable.java
@@ -0,0 +1,19 @@
+package hashtables.lockbased;
+
+import contention.abstractions.CompositionalMap;
+
+import java.lang.invoke.MethodHandles;
+import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
+public class ReduceUnlinearizable extends QueueHashMap<Integer, Integer> {
+    @Override
+    public int size() {
+        return this.reduce(0, Integer::sum);
+    }
+}
