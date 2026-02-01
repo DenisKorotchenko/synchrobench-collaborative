6d2cddd72e3cff38c6e3ed8f3eb79e394c17389f
DenisKorotchenko <dzd1024ozd@gmail.com>
2026-02-01 12:23:47 +0300

    Update

--- java/scripts/run3.sh
+++ java/scripts/run3.sh
@@ -22,7 +22,7 @@ size="67108864"
 #benchs="ru.dksu.semantic.TestStructureShortLong ru.dksu.semantic.TestStructureShortLongRW" # ru.dksu.semantic.ARATestStructureWithout" # ru.dksu.semantic.TestStructureRWSL"
 #benchs="ru.dksu.semantic.TestStructure ru.dksu.semantic.TestStructureRW ru.dksu.semantic.TestStructureSimple ru.dksu.semantic.TestStructureShortLong ru.dksu.semantic.TestStructureShortLongRW"
 # for s in ${snapshots}; do
-benchs="TestStructureWithout"
+benchs="ru.dksu.semantic.TestStructureWithout"
 for t in ${thread}; do
     # for write in ${writes}; do
   for bench in ${benchs}; do
