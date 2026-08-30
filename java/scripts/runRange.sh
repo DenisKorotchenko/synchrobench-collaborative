e21ad34ab7f5630411ed84c26c4724cc01774a5c
DenisKorotchenko <dzd1024ozd@gmail.com>
2026-08-30 17:24:54 +0300

    Update script

--- java/scripts/runRange.sh
+++ java/scripts/runRange.sh
@@ -29,7 +29,7 @@ size="67108864"
 #benchs="ru.dksu.semantic.ExtendedMapSL3_U ru.dksu.semantic.ExtendedMapSL_U"
 #benchs="ru.dksu.semantic.ExtendedMapSLGMER"
 #benchs="ru.dksu.semantic.ExtendedMapSL3_U ru.dksu.semantic.ExtendedMapNoLock ru.dksu.semantic.ExtendedMapRW ru.dksu.semantic.ExtendedMapSL_U ru.dksu.semantic.ExtendedMapSLGME ru.dksu.semantic.ExtendedMapSLGMER"
-benchs="ru.dksu.semantic.SemanticLockRangeV2 ru.dksu.semantic.SemanticLockRangeV3" # ru.dksu.semantic.TestStructureRW ru.dksu.semantic.TestStructureWithout"
+benchs="ru.dksu.semantic.TestStructureRangeV2 ru.dksu.semantic.TestStructureRangeV3" # ru.dksu.semantic.TestStructureRW ru.dksu.semantic.TestStructureWithout"
 #benchs="ru.dksu.semantic.TestStructureGME ru.dksu.semantic.TestStructureGMER" # ru.dksu.semantic.TestStructureRW ru.dksu.semantic.TestStructureWithout"
 #benchs="trees.lockbased.IntegerCollaborativeHelperFairLockBasedStanfordTreeMap trees.lockbased.IntegerLockBasedStanfordTreeMap"
 
